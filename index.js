063f59cd8cbb0253012b77849db3657014bf1b78
Amandine V <amavelamala@gmail.com>
2019-01-24 02:27:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
+/*function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
 
-/*function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({object}, {[key]: value});
   return object, newObj;
 }*/
