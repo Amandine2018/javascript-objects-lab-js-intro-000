d4475d87d3a9ef98b22a4f9012aeb8a0203213ef
Amandine V <amavelamala@gmail.com>
2019-01-24 02:37:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign(object, {[key]: value});
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
